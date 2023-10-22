56448407f212ec96f83081442a07147a4ac2fca2
yuyasat <takiyour@gmail.com>
2023-10-22 21:54:49 +0900

    Checkbox, Radiobox, ListBox, MultiListBox

--- SampleRazorApp/Pages/Other.cshtml.cs
+++ SampleRazorApp/Pages/Other.cshtml.cs
@@ -3,12 +3,36 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SampleRazorApp.Pages
 {
+    public enum Gender
+    {
+        mail,
+        femail
+    }
+    public enum Platform
+    {
+        Windows,
+        macOS,
+        Linux,
+        ChromeOS,
+        Android,
+        iOS
+    }
     public class OtherModel : PageModel
     {
-        public string Message { get; set; }
-        public void OnPost()
+        public string? Message { get; set; }
+        public bool check { get; set; }
+        public Gender gender { get; set; }
+        public Platform pc { get; set; }
+        public Platform[] pc2 { get; set; }
+
+        public void OnGet()
         {
-            Message = "you typed: " + Request.Form["msg"];
+            Message = "check & select it!";
+        }
+
+        public void OnPost(bool check, string gender, Platform pc, Platform[] pc2)
+        {
+            Message = "Result: " + check + "," + gender + ", " + pc + "," + pc2.Length;
         }
     }
 }
