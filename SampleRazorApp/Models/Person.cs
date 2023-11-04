b77867bd5b822202903f7187d1112f77a2e9e7e8
yuyasat <takiyour@gmail.com>
2023-11-04 16:12:23 +0900

    add label name and validation to Person

--- SampleRazorApp/Models/Person.cs
+++ SampleRazorApp/Models/Person.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace SampleRazorApp.Models
 {
     public class Person
     {
-
         public int PersonId { get; set; }
+        [Display(Name = "名前")]
+        [Required(ErrorMessage = "必須項目です。")]
         public string Name { get; set; }
+        [Display(Name = "メールアドレス")]
+        [EmailAddress(ErrorMessage = "メールアドレスが必要です")]
         public string? Mail { get; set; }
+        [Display(Name = "年齢")]
+        [Range(0, 200, ErrorMessage = "0以上、200以下の値にしてください。")]
         public int Age { get; set; }
     }
 }
-
